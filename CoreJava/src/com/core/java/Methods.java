497b12216bdfce95a802b7ae3bf2e95b36f068d5
Mohammed Saif <saifmohammed888@gmail.com>
2020-05-20 18:31:59 +0530

    Methods

--- CoreJava/src/com/core/java/Methods.java
+++ CoreJava/src/com/core/java/Methods.java
@@ -1,23 +1,21 @@
 package com.core.java;
 
 public class Methods {
-	
-	
-	//Method to add two numbers - Implementation
-	public Integer addNumbers(int a,int b) {
-		int sum = a+b;
+
+	// Method to add two numbers - Implementation
+	public Integer addNumbers(int a, int b) {
+		int sum = a + b;
 		return sum;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		System.out.println("Main Method Calling other methods");
-		
+
 		Methods addObject = new Methods();
-		
-		//Method calling
-		int resultSum = addObject.addNumbers(10,15);
-		
-		System.out.println("Result of Sum "+resultSum);
+
+		// Method calling
+		int resultSum = addObject.addNumbers(10, 15);
+
+		System.out.println("Result of Sum " + resultSum);
 	}
 }
