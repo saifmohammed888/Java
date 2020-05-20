b4f903498be0a3aa54c7ad344908b29af3ce6c19
Mohammed Saif <saifmohammed888@gmail.com>
2020-05-20 17:06:57 +0530

    Variables

--- CoreJava/src/com/core/java/Variables.java
+++ CoreJava/src/com/core/java/Variables.java
@@ -1,20 +1,18 @@
 package com.core.java;
 
-public class Variables 							  		//class is a Keyword 
-{
-	public static void main(String args [])  				    
-	{
-		String Name = "Saif"; 	   				 		 //Name is a Variable with String as Data type
-		System.out.println(Name);
-		System.out.println("sum: "+(10+20));   			 //The addition is an operator
-		System.out.println("product: "+10*20);  
-		System.out.println("difference: "+(10-20));  
-		System.out.println("division: "+10/20);  
-		
-		
-		final int a = 10;								 //The integer a is made 	constant
-		System.out.println(a);
-		//a =20;									
-		System.out.println(a);
+public class Variables {
+
+	static String staticVariable = "Static Variable"; 		// --> Static Variable
+	String instanceVariable;
+
+	public static void main(String args[]) {
+		String locaVariabl = "local Variable"; 				// --> Local Variable
+
+		Variables instance = new Variables();
+		instance.instanceVariable = "Instance Variable";	// --> Instance Variable
+
+		System.out.println(staticVariable);
+		System.out.println(locaVariabl);
+		System.out.println(instance.instanceVariable);
 	}
 }
