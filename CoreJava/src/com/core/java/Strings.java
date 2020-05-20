9ee2018fc8e11388bc4055f5d4edf987835c1bf6
Mohammed Saif <saifmohammed888@gmail.com>
2020-05-20 19:48:59 +0530

    Strings

--- CoreJava/src/com/core/java/Strings.java
+++ CoreJava/src/com/core/java/Strings.java
@@ -2,9 +2,17 @@ package com.core.java;
 
 public class Strings {
 
+	
+	public static void Buffer() {
+		
+		StringBuffer buffer = new StringBuffer("hello ");
+		buffer.append("Saif");
+		System.out.println("String buffer "+buffer);
+	}
+	
 	public static void main(String[] args) {
 
-		String name = "  Mohammed Saif";
+		String name = "Mohammed Saif";
 
 		System.out.println(name);
 		System.out.println("Length " + name.length());
@@ -14,5 +22,6 @@ public class Strings {
 		System.out.println("index of " + name.indexOf("s"));
 		System.out.println("Trim leading Spaces " + name.trim());
 
+		Strings.Buffer();
 	}
 }
