d81040d650be6163c79b2608440a9f549495ffaa
Mohammed Saif <saifmohammed888@gmail.com>
2020-05-20 19:30:03 +0530

    Arrays

--- CoreJava/src/com/core/java/Arrays.java
+++ CoreJava/src/com/core/java/Arrays.java
@@ -10,13 +10,41 @@ public class Arrays {
 		System.out.println(a.length);
 		for(int i=0;i<3;i++) {
 			for(int j=0;j<2;j++) {
-				System.out.println(a[i][j]);
+				System.out.println("Multidimensional Array "+a[i][j]);
 			}
 		}
 		
 	}
 	
+	//Activity
+	public static void activityArray(){
+		char ch[] = new char[] {'a','b','c','d'};
+		
+		char copy[]= new char[4];
+		copy = ch.clone();
+		for(char temp:copy) {
+			System.out.println("Copied Array "+temp);
+		}
+		
+		for(int i=1;i<ch.length;i++) {
+			if(i==1) 
+			{
+				for(int j=i+1;i<ch.length-1;i++) {
+					ch[i] = ch[j];
+				}
+			}
+			;
+		}
+		
+		for(char temp:ch) {
+			System.out.println("Deleted Array "+temp);
+		}
+		
+		
+	}
+	
 	//Single Dimensional Arrays
+	
 	public static void main(String[] args) {
 
 		//Array Initialization and Declaration 
@@ -34,5 +62,7 @@ public class Arrays {
 				
 		System.out.println(a.length);
 		Arrays.MultiDimArray();
+		Arrays.activityArray();
 	}
+
 }
