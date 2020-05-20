0a59c22daba73fecd643a445abe0239fbc516515
Mohammed Saif <saifmohammed888@gmail.com>
2020-05-20 18:20:52 +0530

    Flow Control Type casting Operators

--- CoreJava/src/com/core/java/FlowControl.java
+++ CoreJava/src/com/core/java/FlowControl.java
@@ -2,74 +2,85 @@ package com.core.java;
 
 public class FlowControl {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		FlowControl.conditional();
 		FlowControl.looping();
 		FlowControl.transfer();
 	}
-		
-		//Conditional statements
-		
-		static void conditional() 
-		{
-		if(20>10)												//if condition
-		{
-			System.out.println("it is a if conditional statement");		
+
+	// Conditional statements
+	static void conditional() {
+
+		// if condition
+		if (20 > 10) {
+			System.out.println("it is a if conditional statement");
 		}
-		
-		int a =10;
-		if(a>20)
-		{
+
+		// if else condition
+		int a = 10;
+		if (a > 20) {
 			System.out.println("Wont enter");
+		} else if (a < 20) {
+			System.out.println("Else if condition");
 		}
-		else if(a<20)
-		{	
-			System.out.println("Else if condition");				//else if condition
-		}
-		
-		
-	}
 
-		static void looping()
-		{
-			int i=0;
-			while(i<3)													//while loop
-			{
-				System.out.println("while loop");
-				i++;
-			}
-			
-			int j=0;													//do while loop
-			do {
-				 System.out.println("Do while");
-				 j++;
-			   }
-			while(j<3);
-		
-			for(int k=0;k<3;k++) 										//for loop
-			{
-				System.out.println("for loop");
-			}
-		
-		}
-		
-		static void transfer() {
-			int x=3;
-			switch(x) {	
-																							//switch
-			case 1: 
-				System.out.println("Case1");
-				break;
-			case 2: 
-				System.out.println("Case2");
-				break;
-			case 3: 
-				System.out.println("Case3");
-				break;
-			default:
-				System.out.println("No input");
-			}
+		int x = 3;
+		switch (x) {
+		// switch
+		case 1:
+			System.out.println("Case1");
+			break;
+		case 2:
+			System.out.println("Case2");
+			break;
+		case 3:
+			System.out.println("Case3");
+			break;
+		default:
+			System.out.println("No input");
 		}
 	}
 
+	// Looping statements
+	static void looping() {
+
+		// while loop
+		int i = 0;
+		while (i < 3) {
+			System.out.println("while loop");
+			i++;
+		}
+
+		// do while loop
+		int j = 0;
+		do {
+			System.out.println("Do while");
+			j++;
+		} while (j < 3);
+
+		// for loop
+		for (int k = 0; k < 3; k++) {
+			System.out.println("for loop");
+		}
+
+	}
+
+	// Jump statements
+	static void transfer() {
+		int x = 3;
+		switch (x) {
+		// switch
+		case 1:
+			System.out.println("Case1");
+			break;
+		case 2:
+			System.out.println("Case2");
+			break;
+		case 3:
+			System.out.println("Case3");
+			break;
+		default:
+			System.out.println("No input");
+		}
+	}
+}
--- CoreJava/src/com/core/java/Operators.java
+++ CoreJava/src/com/core/java/Operators.java
@@ -1,54 +1,50 @@
 package com.core.java;
 
 public class Operators {
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println("sum: "+10+5);	//Arithmetic operators
-		System.out.println("dif: "+(10-5)); 
-		System.out.println("pro: "+10*5); 
-		System.out.println("quo: "+10/5); 
-		System.out.println("mod: "+10%5); 
-		
-		int a=10;
-		System.out.println(++a);	//unary ++
-		System.out.println(--a);	//unary --
-		
-		int x=100;
-		System.out.println("Assigned Value"+x);		//Simple Assignment operator =
-		int t,y,z; 
-		t=y=z=200;
-		System.out.println("Chained Assigned Value "+t+" "+y+" "+z);	//Chained Assignment operator =
-		
-		int d =x+5;
-		System.out.println("Chained Assigned Value "+d);	//compound Assignment operator =
-				
-		String name="saif";		//String concatination operator
-		String comment=" is the best" ;
-		System.out.println(name+comment);
-		
-		int q=10;
-		System.out.println("Preincrement: "+(++q));	//pre increment
-		System.out.println("predecrement: "+(--q));	//pre decrement										
-		System.out.println("Preincrement: "+(q++));	//post increment										
-		System.out.println("predecrement: "+(q--));	//post decrement										
-		
-		if(true && true)
-		{
-			System.out.println("Logical and operator");	//logical operator
+
+		System.out.println("sum: " + 10 + 5); // Arithmetic operators
+		System.out.println("dif: " + (10 - 5));
+		System.out.println("pro: " + 10 * 5);
+		System.out.println("quo: " + 10 / 5);
+		System.out.println("mod: " + 10 % 5);
+
+		int a = 10;
+		System.out.println(++a); // unary ++
+		System.out.println(--a); // unary --
+
+		int x = 100;
+		System.out.println("Assigned Value" + x); // Simple Assignment operator =
+		int t, y, z;
+		t = y = z = 200;
+		System.out.println("Chained Assigned Value " + t + " " + y + " " + z); // Chained Assignment operator =
+
+		int d = x + 5;
+		System.out.println("Chained Assigned Value " + d); // compound Assignment operator =
+
+		String name = "saif"; // String concatination operator
+		String comment = " is the best";
+		System.out.println(name + comment);
+
+		int q = 10;
+		System.out.println("Preincrement: " + (++q)); // pre increment
+		System.out.println("predecrement: " + (--q)); // pre decrement
+		System.out.println("Preincrement: " + (q++)); // post increment
+		System.out.println("predecrement: " + (q--)); // post decrement
+
+		if (true && true) {
+			System.out.println("Logical and operator"); // logical operator
 		}
-		
-		if (20>10)
-		{
-			System.out.println("Relational operator");	//Relational operator
+
+		if (20 > 10) {
+			System.out.println("Relational operator"); // Relational operator
 		}
-		
-		if (x==100)
-		{
-			System.out.println("Equality operator");	//Equality operator
+
+		if (x == 100) {
+			System.out.println("Equality operator"); // Equality operator
 		}
-		
-	
+
 	}
 
 }
