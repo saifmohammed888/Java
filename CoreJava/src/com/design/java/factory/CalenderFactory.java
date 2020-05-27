40c679df1f9c81646108c22de9b7f565ba0b64d2
Mohammed Saif <saifmohammed888@gmail.com>
2020-05-27 20:18:25 +0530

    Factory

---
+++ CoreJava/src/com/design/java/factory/CalenderFactory.java
@@ -0,0 +1,13 @@
+package com.design.java.factory;
+
+import java.util.Calendar;
+
+public class CalenderFactory {
+
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();
+		System.out.println(cal);
+		System.out.println(cal.get(Calendar.DAY_OF_MONTH));
+	}
+}
