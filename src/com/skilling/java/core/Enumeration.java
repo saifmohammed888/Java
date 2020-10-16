c70e61d1b2f3992bb3ae96363a67fffd1e3273ce
Mohammed Saif <saifmohammed888@gmail.com>
2020-10-17 00:54:39 +0530

    Enumeration

---
+++ src/com/skilling/java/core/Enumeration.java
@@ -0,0 +1,24 @@
+package com.skilling.java.core;
+
+	enum Family {
+		SALMAN, SUHAIB, SAIF, ZOYA;
+
+	public void familyMember() {
+		System.out.println("Method to get family Members");
+
+		System.out.println(Family.SAIF);
+		System.out.println(Family.SALMAN);
+		System.out.println(Family.SUHAIB);
+		System.out.println(Family.ZOYA);
+	}
+	}
+
+	public class Enumeration {
+
+	public static void main(String[] args) {
+
+		Family familyMember = Family.SAIF;
+		System.out.println(familyMember);
+		familyMember.familyMember();
+	}
+	}	
